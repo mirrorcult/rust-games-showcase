c50245bc19072be7512c94965874c6551625e6f2
cyclowns <notzombiedude@gmail.com>
2020-03-03 08:03:46 -0700

    Revert "initial commit"
    This reverts commit a3840e0a318d01969dea8ffe8b03a8aaa220f1d3.

--- chess-basic/target/debug/deps/rust_games_showcase-8356e5ff9671be39.d
+++
@@ -1,5 +0,0 @@
-/media/Personal/Code/github.com/cyclowns/rust-games-showcase/target/debug/deps/rust_games_showcase-8356e5ff9671be39: src/main.rs
-
-/media/Personal/Code/github.com/cyclowns/rust-games-showcase/target/debug/deps/rust_games_showcase-8356e5ff9671be39.d: src/main.rs
-
-src/main.rs:
--- chess-basic/target/debug/rust-games-showcase.d
+++
@@ -1,1 +0,0 @@
-/media/Personal/Code/github.com/cyclowns/rust-games-showcase/target/debug/rust-games-showcase: /media/Personal/Code/github.com/cyclowns/rust-games-showcase/src/main.rs
