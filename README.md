a3840e0a318d01969dea8ffe8b03a8aaa220f1d3
cyclowns <notzombiedude@gmail.com>
2020-03-03 08:02:37 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# rust-games-showcase
+
+a basic showcase of CLI games made with rust
+
+## Games Included (or planned)
+```
+Chess
+Nim
+Battleship
+Minesweeper
+..more
---
+++ chess-basic/target/debug/rust-games-showcase.d
@@ -0,0 +1,1 @@
+/media/Personal/Code/github.com/cyclowns/rust-games-showcase/target/debug/rust-games-showcase: /media/Personal/Code/github.com/cyclowns/rust-games-showcase/src/main.rs
